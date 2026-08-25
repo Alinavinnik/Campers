9c1d043de3c9893e79381cf8a45bd93f2ac98792
Alina <alinavinnikhgfff@gmail.com>
2026-08-25 22:38:31 +0300

    add images

--- app/components/Button/Button.tsx
+++
@@ -1,4 +0,0 @@
-import css from "./Button.module.css";
-export default function Button() {
-  return <></>;
-}
--- app/components/Header/Header.tsx
+++
@@ -1,4 +0,0 @@
-import css from "./Header.module.css";
-export default function Header() {
-  return <></>;
-}
