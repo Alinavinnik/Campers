8d3aa65f389c5bfe501bab46e7beb71e30bc5f65
Alina <alinavinnikhgfff@gmail.com>
2026-08-25 23:03:11 +0300

    add nav behavior

--- components/Header/Header.tsx
+++ components/Header/Header.tsx
@@ -2,20 +2,35 @@
 
 import Link from "next/link";
 import css from "./Header.module.css";
+import { usePathname } from "next/navigation";
 
 export default function Header() {
+  const pathname = usePathname();
+
+  const navLinks = [
+    { name: "Home", href: "/" },
+    { name: "Catalog", href: "/catalog" },
+  ];
+
   return (
     <header className={css.header}>
       <Link href="/" className={css.logo}>
         Travel<span className={css.accent}>Trucks</span>
       </Link>
       <nav>
-        <Link href="/" className={css.logo}>
-          Home
-        </Link>
-        <Link href="/catalog" className={css.logo}>
-          Catalog
-        </Link>
+        {navLinks.map((link) => {
+          const isActive = pathname === link.href;
+
+          return (
+            <Link
+              key={link.href}
+              href={link.href}
+              className={`${css.link} ${isActive ? css.active : ""}`}
+            >
+              {link.name}
+            </Link>
+          );
+        })}
       </nav>
     </header>
   );
