c184f461d30851fd731e7d0ef67e7baef922b213
Alina <alinavinnikhgfff@gmail.com>
2026-08-25 22:53:20 +0300

    add header basic  structure

--- components/Header/Header.tsx
+++ components/Header/Header.tsx
@@ -1,4 +1,22 @@
+"use client";
+
+import Link from "next/link";
 import css from "./Header.module.css";
+
 export default function Header() {
-  return <></>;
+  return (
+    <header className={css.header}>
+      <Link href="/" className={css.logo}>
+        Travel<span className={css.accent}>Trucks</span>
+      </Link>
+      <nav>
+        <Link href="/" className={css.logo}>
+          Home
+        </Link>
+        <Link href="/catalog" className={css.logo}>
+          Catalog
+        </Link>
+      </nav>
+    </header>
+  );
 }
