981e24165fc683498622fed8aef02c07090ef544
Alina <alinavinnikhgfff@gmail.com>
2026-08-26 16:04:00 +0300

    add not-found css

---
+++ components/ClearFilterBtn/ClearFilterBtn.tsx
@@ -0,0 +1,5 @@
+import css from "./ClearFilterBtn.module.css";
+
+export default function ClearFilterBtn() {
+  return <button className={css.button}>Clear filters</button>;
+}
--- components/NotFound/NotFound.tsx
+++ components/NotFound/NotFound.tsx
@@ -1,13 +1,14 @@
-import Link from "next/link";
 import css from "./NotFound.module.css";
 import Image from "next/image";
+
 import ButtonCatalogLink from "../ButtonCatalogLink/ButtonCatalogLink";
+import ClearFilterBtn from "../ClearFilterBtn/ClearFilterBtn";
 export default function NotFound() {
   return (
-    <section>
-      <div className="container">
+    <section className={css["not-found-section"]}>
+      <div className={css.wrapper}>
         <Image
-          src="/images/not-found.jpg"
+          src="/images/not-found.png"
           alt="No campers found"
           width={488}
           height={463}
@@ -15,11 +16,18 @@ export default function NotFound() {
           className={css.img}
         />
         <h1 className={css.title}>No campers found</h1>
-        <p className={css.description}>
-          We couldn`t find any campers that match your filters.Try adjusting
-          your search or clearing some filters.
-        </p>
-        <ButtonCatalogLink>View all campers</ButtonCatalogLink>
+        <div className={css["text-container"]}>
+          <p className={css.description}>
+            We couldn`t find any campers that match your filters.
+          </p>
+          <p className={css.description}>
+            Try adjusting your search or clearing some filters.
+          </p>
+        </div>
+        <div className={css["btn-container"]}>
+          <ClearFilterBtn />
+          <ButtonCatalogLink>View all campers</ButtonCatalogLink>
+        </div>
       </div>
     </section>
   );
