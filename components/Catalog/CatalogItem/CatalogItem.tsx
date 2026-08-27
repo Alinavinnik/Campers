469c369afc8ba339175c7cf6a60dd2532ae08206
Alina <alinavinnikhgfff@gmail.com>
2026-08-27 16:08:03 +0300

    add catalog item css

--- components/Catalog/CatalogItem/CatalogItem.tsx
+++ components/Catalog/CatalogItem/CatalogItem.tsx
@@ -2,9 +2,11 @@
 import Image from "next/image";
 import css from "./CatalogItem.module.css";
 import { Camper } from "@/types/camper";
-import { MdStarRate, MdLocalGasStation } from "react-icons/md";
+import { MdLocalGasStation } from "react-icons/md";
+import { IoStar } from "react-icons/io5";
 import { FaCarAlt } from "react-icons/fa";
 import { TbAutomaticGearbox } from "react-icons/tb";
+import { CiMap } from "react-icons/ci";
 import Link from "next/link";
 
 interface CamperCardProps {
@@ -13,7 +15,11 @@ interface CamperCardProps {
 
 export default function CatalogItem({ camper }: CamperCardProps) {
   const features = [
-    { key: "engine", value: camper.engine, icon: <MdLocalGasStation /> },
+    {
+      key: "engine",
+      value: camper.engine,
+      icon: <MdLocalGasStation />,
+    },
     {
       key: "transmission",
       value: camper.transmission,
@@ -37,24 +43,32 @@ export default function CatalogItem({ camper }: CamperCardProps) {
           className={css.img}
         ></Image>
         <div className={css.info}>
-          <h2 className={css.title}>{camper.name}</h2>
-          <p className={css.price}>€{camper.price}</p>
-          <div className={css.rating}>
-            <MdStarRate />
-            {camper.rating}
-            <span>({camper.totalReviews} Reviews)</span>
+          <div className={css["top-row"]}>
+            <h2 className={css.title}>{camper.name}</h2>
+            <p className={css.price}>€{camper.price}</p>
           </div>
-          <div className={css.location}>{camper.location}</div>
+          <div className={css["second-row"]}>
+            <div className={css.rating}>
+              <IoStar size={16} color={"var(--rating)"} className={css.icon} />
+              {camper.rating}
+              <span>({camper.totalReviews} Reviews)</span>
+            </div>
+            <div className={css.location}>
+              <CiMap size={18} className={css.icon} />
+              {camper.location}
+            </div>
+          </div>
           <p className={css.description}>{camper.description}</p>
           <ul className={css.badges}>
             {features.map((f) => (
               <li key={f.key} className={css.badge}>
-                {f.icon}
+                <span className={css.iconWrapper}>{f.icon}</span>
+
                 {f.value}
               </li>
             ))}
           </ul>
-          <span className="btn">Show more</span>
+          <span className={css.btn}>Show more</span>
         </div>
       </Link>
     </li>
