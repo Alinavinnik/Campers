dd5ace88019027e2d74d751c8ccfc430183b467b
Alina <alinavinnikhgfff@gmail.com>
2026-08-28 18:21:50 +0300

    add review component

--- components/CamperDetale/CamperReviews/CamperReviews.tsx
+++ components/CamperDetale/CamperReviews/CamperReviews.tsx
@@ -1,5 +1,6 @@
 import { fetchReviews } from "@/services/camperService";
 import css from "./CamperReviews.module.css";
+import { IoStar } from "react-icons/io5";
 
 interface CamperReviewsProps {
   camperId: string;
@@ -11,11 +12,29 @@ export default async function CamperReviews({ camperId }: CamperReviewsProps) {
   return (
     <div className={css.reviews}>
       <h2 className={css.title}>Reviews</h2>
-      {data.map((review) => {
+      {data.map((r) => {
         return (
-          <div key={review.id}>
-            <p>{review.reviewer_name}</p>
-            <p>{review.comment}</p>
+          <div className={css["review-card"]} key={r.id}>
+            <div className={css["top-content"]}>
+              <div className={css.round}>{r.reviewer_name[0]}</div>
+              <div className={css.wrap}>
+                <p className={css.name}>{r.reviewer_name}</p>
+                <div className={css.rating}>
+                  {[1, 2, 3, 4, 5].map((star) => (
+                    <IoStar
+                      key={star}
+                      size={16}
+                      color={
+                        star <= r.reviewer_rating
+                          ? "var(--rating)"
+                          : "var(--gray-light)"
+                      }
+                    />
+                  ))}
+                </div>
+              </div>
+            </div>
+            <p className={css.comment}>{r.comment}</p>
           </div>
         );
       })}
